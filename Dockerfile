fd1b7df1fa937db2a74c20b56f347975220c89b5
IronicBadger <alexktz@gmail.com>
2015-11-10 09:35:07 +0000

    Tidied up files ready for release testing, added docs

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 FROM linuxserver/baseimage
 MAINTAINER LinuxServer.io <ironicbadger@linuxserver.io>
 
-ENV APTLIST="snapraid python2.7 git"
+ENV APTLIST="snapraid python2.7 git screen"
 
-RUN add-apt-repository ppa:tikhonov/snapraid
-RUN apt-get update -qq
-RUN apt-get install $APTLIST -qy
-RUN mkdir -p /app && cd /app && git clone https://github.com/Chronial/snapraid-runner.git && cp snapraid-runner/snapraid-runner.conf.example /defaults/snapraid-runner.conf.example
-RUN chmod +x /app/snapraid-runner/snapraid-runner.py
-    #apt-get clean && rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/*
+RUN add-apt-repository ppa:tikhonov/snapraid && \
+    apt-get update -qq && \
+    apt-get install $APTLIST -qy && \
+    mkdir -p /app && \
+    cd /app && \
+    git clone https://github.com/Chronial/snapraid-runner.git && \
+    cp snapraid-runner/snapraid-runner.conf.example /defaults/snapraid-runner.conf.example && \
+    chmod +x /app/snapraid-runner/snapraid-runner.py && \
+    apt-get clean && rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/*
 
 #Adding Custom files
 ADD init/ /etc/my_init.d/
--- init/50_check_config.sh
+++ init/50_check_config.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+# Ensures that configuration files for both SnapRAID and snapraid-runner are present
+# in /config. In reality, both files should be edited manually before running this
+# container to ensure correct operation.
 
 # test for /etc/snapraid.conf being a file and not a link, delete if file.
 [[ ! -L /etc/snapraid.conf && -f /etc/snapraid.conf ]] && rm /etc/snapraid.conf
 
 # test if snapraid.conf is in /config, copy from /defaults/snapraid.conf.example if not.
-[[ ! -f /config/snapraid.conf ]] && (cp /defaults/snapraid.conf.example /config/snapraid.conf && echo "No config found, copying default...")
+[[ ! -f /config/snapraid.conf ]] && \
+  (cp /defaults/snapraid.conf.example /config/snapraid.conf && \
+  echo "No config found, copying default...")
+
+[[ ! -f /config/snapraid-runner.conf ]] && \
+  (cp /defaults/snapraid-runner.conf.example /config/snapraid-runner.conf) && \
+  echo "Runner config not found, copying default - please edit"
 
 # test if link is made between /etc/snapraid.conf and /config/snapraid.conf, make if not
 [[ ! -L /etc/snapraid.conf ]] && ln -s /config/snapraid.conf /etc/snapraid.conf
 
-# chown local copy of /config/snapraid.conf
-chown abc:abc /config/snapraid.conf
+# chown local copies of /config/*.conf and /app
+chown -R abc:abc /app /config
