f5ed809ca946421126fae13fb242d36454d1b895
Alex Kretzschmar <alexktz@gmail.com>
2015-11-16 14:15:34 +0100

    Refactor attempt

--- init/40_inject_cron.sh
+++ init/40_inject_cron.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Inject user supplied cron files
-[[ ! -d /config/cron ]] && \
-  (echo "No cron jobs found to inject..." && \
-	exit 0)
-
 # Delete cron jobs except those supplied with this container
 find /etc/cron.d/ -type f -not -name 'snapraid-runner' -delete
 
-# inject user supplied cron files
-for f in /config/cron/* ; do
-    echo "injecting: $f"
-    cp $f /etc/cron.d/
-done
+# Inject user supplied cron files
+[[ -d /config/cron ]] && \
+  (for f in /config/cron/* ; do
+      echo "injecting: $f"
+      cp $f /etc/cron.d/
+  done)
